9c3affa02c269609b1d3db42ac58d7d09f8dcbc1
Ekin Yilmaz <ekin.yilmaz@tum.de>
2019-07-16 13:28:36 +0000

    Upload New File

---
+++ road.h
@@ -0,0 +1,19 @@
+#ifndef ROAD_H
+#define ROAD_H
+
+
+class road
+{
+public:
+    road(int baslangic_x=0,
+    int baslangic_y=0,
+    int son_x=0,
+    int son_y=0);
+
+    int baslangic_x;
+    int baslangic_y;
+    int son_x;
+    int son_y;
+};
+
+#endif // ROAD_H
