eb421b5d2cb74ebe4eb3e81838a5011dac3414c0
Ekin Yilmaz <ekin.yilmaz@tum.de>
2019-07-16 13:21:31 +0000

    Upload New File

---
+++ decor.cpp
@@ -0,0 +1,35 @@
+#include "decor.h"
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
+
+decor::decor(QGraphicsItem * parent,int png_nmbr): QGraphicsPixmapItem(parent)
+{
+    if(png_nmbr==1){
+    this->pixmap = QPixmap(":/map_images/tree.png");
+    }
+    if(png_nmbr==2){
+    this->pixmap = QPixmap(":/map_images/Rock_01.png");
+    }
+    if(png_nmbr==3){
+    this->pixmap = QPixmap(":/map_images/grandstandYARIM.png");
+    }
+    if(png_nmbr==4){
+    this->pixmap = QPixmap(":/map_images/Water_Tile.png");
+    }
+    if(png_nmbr==5){
+    this->pixmap = QPixmap(":/map_images/Pavilion_01.png");
+    }
+    if(png_nmbr==6){
+    this->pixmap = QPixmap(":/map_images/Pavilion_02.png");
+    }
+    if(png_nmbr==7){
+    this->pixmap = QPixmap(":/map_images/Soil_Tile.png");
+    }
+
+    drawOrmanKanunlariFero();
+}
+void decor::drawOrmanKanunlariFero()
+
+{
+   setPixmap(this->pixmap);
+}
