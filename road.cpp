46a459ba831f37bef6b1933bba21eb3d74d1f2d2
Ekin Yilmaz <ekin.yilmaz@tum.de>
2019-07-16 13:28:28 +0000

    Upload New File

---
+++ road.cpp
@@ -0,0 +1,14 @@
+#include "road.h"
+
+road::road(int baslangic_x,
+           int baslangic_y,
+           int son_x,
+           int son_y)
+{
+    this->baslangic_x = baslangic_x;
+    this->baslangic_y = baslangic_y;
+    this->son_x = son_x;
+    this->son_y = son_y;
+
+
+}
